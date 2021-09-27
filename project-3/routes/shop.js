62062476bb5083ddeed0bf4ab19838a9237c860d
Nilay <54245048+dnilay@users.noreply.github.com>
2021-09-27 14:42:37 +0530

    Add files via upload

--- project-3/routes/shop.js
+++ project-3/routes/shop.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.send('<h1>Hello from Express!</h1>');
+  res.send('<h1>Hello from Express!</h1><br/><a href="/admin/add-product">Add A New Product</a>');
 });
 
 module.exports = router;
