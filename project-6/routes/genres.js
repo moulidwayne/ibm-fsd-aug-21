b9446fe2fdd56aeade3264dc6e1625d1c4b7ede7
Nilay <54245048+dnilay@users.noreply.github.com>
2021-09-30 15:18:20 +0530

    Add files via upload

---
+++ project-6/routes/genres.js
@@ -0,0 +1,41 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+const express = require('express');
+const router = express.Router();
+
+const Genre = mongoose.model('Genre', new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 50
+  }
+}));
+
+router.get('/', async (req, res) => {
+  const genres = await Genre.find().sort('name');
+  res.send(genres);
+});
+
+router.post('/', async (req, res) => {
+  const { error } = validateGenre(req.body); 
+  if (error)
+  {
+    return res.status(400).send(error.details[0].message);
+  } 
+
+  let genre = new Genre({ name: req.body.name });
+  genre = await genre.save();
+  
+  res.send(genre);
+});
+
+function validateGenre(genre) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  return Joi.validate(genre, schema);
+}
+
+module.exports = router;
