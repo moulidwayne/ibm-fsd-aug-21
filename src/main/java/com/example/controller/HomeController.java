8124aecb235462a240c36087a80c5efd4c921b91
nilay <mailnilaydas@gmail.com>
2021-10-19 10:49:38 +0530

    first commit

---
+++ src/main/java/com/example/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String sayHello()
+    {
+        return "index";
+    }
+    @RequestMapping("/hello")
+    public String hello()
+    {
+        return "hello";
+    }
+
+}
---
+++ src/main/webapp/WEB-INF/view/hello.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: nilay
+  Date: 19/10/21
+  Time: 10:41 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+this is hello page
+</body>
+</html>
