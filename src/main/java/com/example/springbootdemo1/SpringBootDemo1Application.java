b32bf9cf44cd57e30ed12d4c1084cac8ec9044ab
nilay <mailnilaydas@gmail.com>
2021-10-20 17:35:37 +0530

    first commit

---
+++ src/main/java/com/example/springbootdemo1/SpringBootDemo1Application.java
@@ -0,0 +1,15 @@
+package com.example.springbootdemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+
+public class SpringBootDemo1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemo1Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/springbootdemo1/controller/Hello.java
@@ -0,0 +1,12 @@
+package com.example.springbootdemo1.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class Hello {
+    @RequestMapping("/")
+    public String sayHello()
+    {
+        return "index";
+    }
+}
