4e38adad6b61359e40317efc5d5fd705ccdfdbc2
Nilay <54245048+dnilay@users.noreply.github.com>
2021-10-26 09:59:13 +0530

    Add files via upload

---
+++ product-service/src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+	private Environment environment;
+	
+	@Autowired
+	public ProductController(Environment environment) {
+		this.environment = environment;
+	}
+
+	@GetMapping
+	public ResponseEntity<String> productHome()
+	{
+		return ResponseEntity.ok("product service is up and running on port: "+environment.getProperty("local.server.port"));
+	}
+
+}
